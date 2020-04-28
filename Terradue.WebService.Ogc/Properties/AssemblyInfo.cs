beda0b62d1636f593c25d55447c79f8448c7eaa4
eboissier <enguerran.boissier@terradue.com>
2020-04-28 10:47:35 +0200

    Add Assembly info file

---
+++ Terradue.WebService.Ogc/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using NuGet4Mono.Extensions;
+
+[assembly: AssemblyTitle("Terradue.WebService.Ogc")]
+[assembly: AssemblyDescription("Terradue WebService Ogc .Net Standard library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Terradue")]
+[assembly: AssemblyProduct("Terradue.WebService.Ogc")]
+[assembly: AssemblyCopyright("Terradue")]
+[assembly: AssemblyAuthors("Enguerran Boissier")]
+[assembly: AssemblyProjectUrl("https://github.com/Terradue/DotNetOgcWebService")]
+[assembly: AssemblyLicenseUrl("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyInformationalVersion("0.1")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
