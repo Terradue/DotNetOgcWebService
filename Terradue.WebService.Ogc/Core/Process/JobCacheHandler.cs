db85fe03620811402c86d95f71d2325f69f96ba9
eboissier <enguerran.boissier@terradue.com>
2020-04-28 12:15:35 +0200

    missing file

---
+++ Terradue.WebService.Ogc/Core/Process/JobCacheHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Terradue.WebService.Ogc.Core {
+    public abstract class JobCacheHandler {
+
+        public JobCacheHandler() {
+        }
+
+        public virtual void Save(string uid, JobOrder order) {
+
+        }
+
+        public virtual void Load(string uid, JobOrder order) {
+
+        }
+    }
+}
